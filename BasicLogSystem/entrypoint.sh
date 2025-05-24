aa47440556fb0ab76c4d3b0da7474fad64fee230
Largo47 <w.szczepanski47@gmail.com>
2025-05-24 19:17:10 +0200

    semi automate creation of superuser

--- BasicLogSystem/entrypoint.sh
+++ BasicLogSystem/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Collect static files if needed
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Migrate DB if changes needed
+# Make initial DB migration
 python manage.py migrate --no-input
+
+# Add a default super user
+python manage.py createsuperuser --no-input --username root --email nothing@nothing.com || true
