aa25a74165caab2520cd9a06add492735da344f2
Largo47 <w.szczepanski47@gmail.com>
2025-05-27 07:11:51 +0200

    Small fix to url syntax

--- BasicLogSystem/LogHandlerApp/urls.py
+++ BasicLogSystem/LogHandlerApp/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     path("api/", views.api, name="api list"),
     path("projects/", views.projects, name="All projects"),
     path("projects/<str:bin_name>/", views.project_tickets, name="Project"),
-    path("projects/<str:bin_name>/issues/<int:issue_id>", views.ticket, name="Issue"),
+    path("projects/<str:bin_name>/issues/<int:issue_id>/", views.ticket, name="Issue"),
 
     ##### APIs ######
     path('api/projects', views.RestProjects, name='Projects'),
-    path('api/projects/<str:bin_name>/issues', views.RestIssues, name='Ticket'),
-    path('api/projects/<str:bin_name>/issues/<int:issue_id>', views.RestIssueByID, name='Ticket'),
-    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs', views.RestLog, name='Logs'),
-    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>', views.RestLogByID, name='Logs'),
-    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>/datetime', views.RestLogByID_time, name='Logs'),
-    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>/line', views.RestLogByID_line, name='Logs'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/projects/<str:bin_name>/issues/', views.RestIssues, name='Ticket'),
+    path('api/projects/<str:bin_name>/issues/<int:issue_id>/', views.RestIssueByID, name='Ticket'),
+    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/', views.RestLog, name='Logs'),
+    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>/', views.RestLogByID, name='Logs'),
+    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>/datetime/', views.RestLogByID_time, name='Logs'),
+    path('api/projects/<str:bin_name>/issues/<int:issue_id>/logs/<int:log_id>/line/', views.RestLogByID_line, name='Logs'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
